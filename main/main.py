086ecc473a3c0cbc47e7ded4073c5b2905a1b574
EdgarTrejoh <edgatrejoh1@gmail.com>
2023-11-10 13:47:27 -0600

    se actualizan indicadores y ráficas

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-# Import the required libraries
+# 1. Instalar las librerías
 
 import streamlit as st
 import yfinance as yf
@@ -9,13 +9,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import plotly.graph_objects as go
 
-# Define the corresponding ticker for the company
+# 2. Definir las variables
 ticker = st.text_input("Ticker:", key="ticker")
 
 st.write("Linear Regression")
 
 st.write("https://pypi.org/project/yfinance/")
 
+# 3. Generar la información:
 if ticker !="" :   
     data = yf.download(ticker, period = "5Y", progress = False)
     data_2022 = data.loc["07-2023":]
@@ -25,7 +26,9 @@ if ticker !="" :
 else:
         st.write("Nothing to show")
 
-#Cálculos:
+# 4. Realizar los Cálculos:
+# 4.1 Modelo de Regresión lineal:
+
 data['Numbers']  = list(range(0, len(data)))
 
 X = np.array(data[['Numbers']])
@@ -39,25 +42,56 @@ y_pred = lin_model.coef_ * X + lin_model.intercept_
 
 data['Pred'] = y_pred
 
-data['Crec_Anual'] = data.Close.div(data.Close.iloc[0]).mul(100)
+# 4.2 Crecimiento base - 5Y
+data['IncBase5Y'] = data.Close.div(data.Close.iloc[0]).mul(100)
 
-# Crate the graphics 
-st.line_chart(data, x= 'Date', y='Crec_Anual', color="#FF0000")
+# 4.3 Rendimiento anual
 
-st.line_chart(data, x= 'Date', y=['Close'])
+data['DeltaD'] = data.Close.pct_change(1)
+rendimiento_anual_simple = round(data['DeltaD'].mean()*252, 2)*100
+
+# 4.4 CARG
+
+CAGR = round((((data['Close'].iloc[-1] / (data['Close'].iloc[0])) ** (1/ len(data['Close'])) -1)*100)*252,2)
+
+# 4.5 Varianza
+
+var = data['DeltaD'].var()
+volatilidad = round(data['DeltaD'].std()*252**0.5,4)*100
+
+
+
+st.write(f"Rendimiento histórico anual simple es de: " + str(rendimiento_anual_simple)+ "%")
+
+st.write(f"La volatildiad anual es de: " + str(volatilidad)+ "%")
+
+st.write(f"CAGR: " + str(CAGR)+ "%")
+
+metricas = data['Close'].loc['2023': ].describe() 
+
+st.markdown("**Estadísticas 2023 - Precio de Cierre**")
+
+st.text(metricas)
+
+st.markdown("**Gráficas**")
+
+# 5. Generar las gráficas:
+
+st.line_chart(data, x= 'Date', y='IncBase5Y', color="#FF0000")
+
+st.line_chart(data, x= 'Date', y=['Close'], color="#338AFF")
 
 st.write("Predictor")
 fig, ax = plt.subplots() 
 data['Pred'].plot(ax=ax, linestyle = "-", lw=2)
 data['Close'].plot(ax=ax, lw=2)
-
 ax.set_title(f'Predict prices: {ticker}')
 st.pyplot(fig)
 
 st.write(r2_score(data['Close'], data['Pred']))
 st.write(lin_model.coef_ * len(data) + 5 + lin_model.intercept_)
 
-# Candlesticks
+# 5.3 Candlesticks
 figura  = go.Figure(data = [go.Candlestick(x=data_2022['Date'], 
                                 open = data_2022['Open'],
                                 high = data_2022['High'],
@@ -66,9 +100,12 @@ figura  = go.Figure(data = [go.Candlestick(x=data_2022['Date'],
 
 st.plotly_chart(figura, use_container_width = True)
 
+# 5.4 Rendimientos
 
-
-
+datos = data['DeltaD']
+figura_2, ejes = plt.subplots()
+ejes.hist(datos, bins=40, color="#FF4C33", edgecolor="#635E5D")
+figura_2
 
 if st.checkbox("Show Dataframe and Chart"):
     chart_data = pd.DataFrame(
