7d10b82e862d80c75682618593e9e4522e5c60af
EdgarTrejoh <140085430+EdgarTrejoh@users.noreply.github.com>
2023-11-29 21:02:48 -0600

    Actualizar 2_📊_FundamentalAnalysis.py

--- main/pages/2_📊_FundamentalAnalysis.py
+++ main/pages/2_📊_FundamentalAnalysis.py
@@ -8,6 +8,15 @@ st.set_page_config(
 
 st.markdown("# :green[2. Fundamental Analysis 📊 ]")
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 "----------"
 
 image = Image.open("./main/img/workin.gif")
